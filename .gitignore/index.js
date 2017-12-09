70df33467073878b1515fb607125792f8a736957
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2017-12-09 13:58:38 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,32 @@ bot.login("Mzg4NzM2NzY2MzU5NjMzOTIx.DQ1VwQ.bBA9lVDyF6G4q33wPkDlfuNUyKY");
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Test: \n -.help");
+        message.channel.sendMessage("Liste des commandes: \n - .insta = Insta de Carla \n - .youtube = Chaîne de Carla \n - .newvideo = Nouvelle vidéo de Carla ! \n *Ce bot contient des commandes cachées :)*");
+        console.log("Help effectué !");
     }
-});
+
+    if (message.content === prefix + "insta"){
+        message.channel.sendMessage("Instagram de Carla: Inconnue pour l'instant /!\ Contacter MhBurgerKing pour afficher sur cette commande");
+        console.log("Insta effectué");
+    }
+
+    if (message.content === prefix + "youtube"){
+        message.channel.sendMessage("Chaîne YouTube de Carla: https://www.youtube.com/channel/UCqFI3r7Rlsdw6AVge-zvKfA");
+        console.log("YouTube effectué");
+    }
+
+    if (message.content === prefix + "fabriquant"){
+        message.channel.sendMessage("Ce bot a été crée par MhBurgerKing.");
+        console.log("Fabriquant effectué");
+    }
+
+    if (message.content === prefix + "carla"){
+        message.channel.sendMessage("Je m'appelle Carla j'ai 14 ans/ Micro: Blue Yeti/ Cam: WebCam Logitech C922/ Pc: AlienWare 17/ \n Logiciel de montage: SonyVegas Pro 14 \n carlaasmr38@gmail.com");
+        console.log("carla effectué");
+    }
+
+    if (message.content === prefix + "newvideo"){
+        message.channel.sendMessage("Cette commande est en développement /!\ Chaîne de Carla: https://www.youtube.com/channel/UCqFI3r7Rlsdw6AVge-zvKfA ");
+        console.log("newvideo effectué");
+    }
 
