3f9092d42db3f0d8a69091cc28651b933a9a822b
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2017-12-09 13:22:23 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,6 +12,12 @@ bot.on('message', message => {
     if(message.content[0] === PREFIX) {
         if(message.content === '.help') {
             message.channel.send('Liste des commandes: .insta , .youtube , / Ce bot contient des commandes cachées :)');
+    if(message.content[0] === PREFIX) {
+        if(message.content === '.insta') {
+            message.channel.send('Instagram de Carla ASMR:');
+    if(message.content[0] === PREFIX) {
+        if(message.content === '.youtube') {
+            message.channel.send('Chaîne YouTube de Carla ASMR: https://www.youtube.com/channel/UCqFI3r7Rlsdw6AVge-zvKfA');
         
         }
     }
