dcb0c9f325cc47cad3a09c3507b5dd54fc64dd2c
MhBurgerKing <34395040+MhBurgerKing@users.noreply.github.com>
2017-12-09 13:39:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,21 +1,19 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const PREFIX = ".";
+var prefix = (".");
+
 bot.on('ready', function() {
-    bot.user.setGame("CarlaAsmrBot, .help");
+    bot.user.setGame("By MhBurgerKing. Command: .help");
     console.log("Le bot a bien ete connecte");
 });
 
+bot.login("Mzg4NzM2NzY2MzU5NjMzOTIx.DQ1VwQ.bBA9lVDyF6G4q33wPkDlfuNUyKY");
+
 
 bot.on('message', message => {
-    if(message.content[0] === PREFIX) {
-        if(message.content === '.help') {
-            message.channel.send('Liste des commandes: .insta , .youtube , / Ce bot contient des commandes cachées :)');
-        
-        
-        }
+    if (message.content(prefix + "help")){
+        message.channel.sendMessage("Test: \n -.help");
     }
 });
 
-bot.login("Mzg4NzM2NzY2MzU5NjMzOTIx.DQ1VwQ.bBA9lVDyF6G4q33wPkDlfuNUyKY");
